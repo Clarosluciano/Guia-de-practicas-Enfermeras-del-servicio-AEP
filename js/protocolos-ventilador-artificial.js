4b007672fc03a34510387a3b2757ebdb70b24e5c
Luciano Claros <luchomon30@gmail.com>
2023-04-04 20:38:34 -0300

    js fuctionalities added, mobile version ended 4

--- js/protocolos-ventilador-artificial.js
+++ js/protocolos-ventilador-artificial.js
@@ -1,48 +1,69 @@
 window.onload = function () {
-
-    var main = document.getElementById('MAIN');
+    //------------- ELEMENTS
     var sectionOne = document.getElementById('SECTION_ONE');
     var sectionTwo = document.getElementById('SECTION_TWO');
     var sectionThree = document.getElementById('SECTION_THREE');
     var sectionFour = document.getElementById('SECTION_FOUR');
     var sectionFive = document.getElementById('SECTION_FIVE');
     var sectionSix = document.getElementById('SECTION_SIX');
+
+    var button = document.getElementsByClassName('button');
+
     var protocolsContainer = document.getElementsByClassName('protocols-container');
 
+    //------------- LISTENERS
     sectionOne.addEventListener('click', (e) => {
         if (e.target && e.target.className === 'button') {
-            protocolsContainer[0].classList.toggle('active');
-            e.target.classList.toggle('button-little');
+            protocolsContainer[0].classList.add('active');
+            e.target.classList.add('button-little');
+        } else if (e.target && e.target.className === 'button button-little') {
+            protocolsContainer[0].classList.remove('active');
+            button[0].classList.remove('button-little');
         }
     })
     sectionTwo.addEventListener('click', (e) => {
         if (e.target && e.target.className === 'button') {
             protocolsContainer[1].classList.add('active');
             e.target.classList.add('button-little');
+        } else if (e.target && e.target.className === 'button button-little') {
+            protocolsContainer[1].classList.remove('active');
+            button[1].classList.remove('button-little');
         }
     })
     sectionThree.addEventListener('click', (e) => {
         if (e.target && e.target.className === 'button') {
-            protocolsContainer[2].classList.toggle('active');
-            e.target.classList.toggle('button-little');
+            protocolsContainer[2].classList.add('active');
+            e.target.classList.add('button-little');
+        } else if (e.target && e.target.className === 'button button-little') {
+            protocolsContainer[2].classList.remove('active');
+            button[2].classList.remove('button-little');
         }
     })
     sectionFour.addEventListener('click', (e) => {
         if (e.target && e.target.className === 'button') {
-            protocolsContainer[3].classList.toggle('active');
-            e.target.classList.toggle('button-little');
+            protocolsContainer[3].classList.add('active');
+            e.target.classList.add('button-little');
+        } else if (e.target && e.target.className === 'button button-little') {
+            protocolsContainer[3].classList.remove('active');
+            button[3].classList.remove('button-little');
         }
     })
     sectionFive.addEventListener('click', (e) => {
         if (e.target && e.target.className === 'button') {
-            protocolsContainer[4].classList.toggle('active');
-            e.target.classList.toggle('button-little');
+            protocolsContainer[4].classList.add('active');
+            e.target.classList.add('button-little');
+        } else if (e.target && e.target.className === 'button button-little') {
+            protocolsContainer[4].classList.remove('active');
+            button[4].classList.remove('button-little');
         }
     })
     sectionSix.addEventListener('click', (e) => {
         if (e.target && e.target.className === 'button') {
-            protocolsContainer[5].classList.toggle('active');
-            e.target.classList.toggle('button-little');
+            protocolsContainer[5].classList.add('active');
+            e.target.classList.add('button-little');
+        } else if (e.target && e.target.className === 'button button-little') {
+            protocolsContainer[5].classList.remove('active');
+            button[5].classList.remove('button-little');
         }
     })
 
