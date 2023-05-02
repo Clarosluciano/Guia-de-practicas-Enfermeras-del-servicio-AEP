00b99e6dcca1b82d86dde92329c12b1ccbb2ad41
Luciano Claros <luchomon30@gmail.com>
2023-05-02 16:40:26 -0300

    new structure added

---
+++ js/landing.js
@@ -0,0 +1,11 @@
+function redirectionAVM() {
+    location.href = "protocolos-ventilador-artificial.html"
+}
+
+function redirectionQuemados() {
+    location.href = "protocolos-quemados.html"
+}
+
+function redirectionStroke() {
+    location.href = ""
+}
