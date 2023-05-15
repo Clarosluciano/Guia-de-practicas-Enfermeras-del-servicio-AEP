3d0e3cdc31d30d5536f0afed4cf93688b62e5ad6
Luciano Claros <luchomon30@gmail.com>
2023-05-15 16:46:11 -0300

    stroke files still working 3

--- js/protocolos-stroke.js
+++ js/protocolos-stroke.js
@@ -6,6 +6,8 @@ window.onload = function () {
     let sectionOne = document.getElementById('SECTION_ONE');
     let sectionTwo = document.getElementById('SECTION_TWO');
     let sectionThree = document.getElementById('SECTION_THREE');
+    let sectionFour = document.getElementById('SECTION_FOUR');
+    let sectionFive = document.getElementById('SECTION_FIVE');
 
     let button = document.getElementsByClassName('button');
     let protocolsContainer = document.getElementsByClassName('protocols-container');
@@ -42,6 +44,24 @@ window.onload = function () {
         } else if (e.target && e.target.className === 'button button-little') {
             protocolsContainer[2].classList.remove('active');
             button[2].classList.remove('button-little');
+        }
+    })
+    sectionFour.addEventListener('click', (e) => {
+        if (e.target && e.target.className === 'button') {
+            protocolsContainer[3].classList.add('active');
+            e.target.classList.add('button-little');
+        } else if (e.target && e.target.className === 'button button-little') {
+            protocolsContainer[3].classList.remove('active');
+            button[3].classList.remove('button-little');
+        }
+    })
+    sectionFive.addEventListener('click', (e) => {
+        if (e.target && e.target.className === 'button') {
+            protocolsContainer[4].classList.add('active');
+            e.target.classList.add('button-little');
+        } else if (e.target && e.target.className === 'button button-little') {
+            protocolsContainer[4].classList.remove('active');
+            button[4].classList.remove('button-little');
         }
     })
 }
