73b4a926be5c890aeae386171f96ec27075ca5cf
Luciano Claros <luchomon30@gmail.com>
2023-05-24 17:42:02 -0300

    biblio section work in progress

--- js/biblio.js
+++ js/biblio.js
@@ -1,4 +1,48 @@
-//Function anti theft of intellectual property
-$("body").bind('cut copy paste', function (e) {
-    e.preventDefault();
-})
+window.onload = function () {
+    //------------- ELEMENTS
+    let sectionOne = document.getElementById('SECTION_ONE');
+    let sectionTwo = document.getElementById('SECTION_TWO');
+    let sectionThree = document.getElementById('SECTION_THREE');
+    let sectionFour = document.getElementById('SECTION_FOUR');
+
+    let button = document.getElementsByClassName('button');
+    let protocolsContainer = document.getElementsByClassName('protocols-container');
+
+    //------------- LISTENERS
+    sectionOne.addEventListener('click', (e) => {
+        if (e.target && e.target.className === 'button') {
+            protocolsContainer[0].classList.add('active');
+            e.target.classList.add('button-little');
+        } else if (e.target && e.target.className === 'button button-little') {
+            protocolsContainer[0].classList.remove('active');
+            button[0].classList.remove('button-little');
+        }
+    })
+    sectionTwo.addEventListener('click', (e) => {
+        if (e.target && e.target.className === 'button') {
+            protocolsContainer[1].classList.add('active');
+            e.target.classList.add('button-little');
+        } else if (e.target && e.target.className === 'button button-little') {
+            protocolsContainer[1].classList.remove('active');
+            button[1].classList.remove('button-little');
+        }
+    })
+    sectionThree.addEventListener('click', (e) => {
+        if (e.target && e.target.className === 'button') {
+            protocolsContainer[2].classList.add('active');
+            e.target.classList.add('button-little');
+        } else if (e.target && e.target.className === 'button button-little') {
+            protocolsContainer[2].classList.remove('active');
+            button[2].classList.remove('button-little');
+        }
+    })
+    sectionFour.addEventListener('click', (e) => {
+        if (e.target && e.target.className === 'button') {
+            protocolsContainer[3].classList.add('active');
+            e.target.classList.add('button-little');
+        } else if (e.target && e.target.className === 'button button-little') {
+            protocolsContainer[3].classList.remove('active');
+            button[3].classList.remove('button-little');
+        }
+    })
+}
